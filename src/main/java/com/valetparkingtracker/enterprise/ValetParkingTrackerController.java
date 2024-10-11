86576c3ffe4fdf4239a3369c05efb8e4a390797b
whsiq <staggsce@mail.uc.edu>
2024-10-11 14:55:14 -0400

    created html templates and controller mappings

--- src/main/java/com/valetparkingtracker/enterprise/ValetParkingTrackerController.java
+++ src/main/java/com/valetparkingtracker/enterprise/ValetParkingTrackerController.java
@@ -3,8 +3,19 @@ package com.valetparkingtracker.enterprise;
 import com.valetparkingtracker.enterprise.dto.Vehicle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * The controller for Valet Parking Tracker REST endpoints and web UI
+ * <p>
+ *     This class handles the CRUD operations for Valet Parking Tracker vehicles through HTTP actions
+ * </p>
+ * <p>
+ *     This class also serves HTML pages for UI interactions with vehicles
+ * </p>
+ */
 @Controller
 public class ValetParkingTrackerController {
 
@@ -25,4 +36,40 @@ public class ValetParkingTrackerController {
         return "start";
     }
 
+    /**
+     * Should eventually save a vehicle to the database and return to the start page
+     * @param vehicle the vehicle to be saved with the provided information
+     * @return
+     */
+    @RequestMapping("/saveVehicle")
+    public String saveVehicle(Vehicle vehicle) {
+        return "start";
+    }
+
+    /**
+     * Displays the form to add a new vehicle
+     *
+     * @return the newVehicle html page
+     */
+    @RequestMapping("/newVehicle")
+    public String addVehicle(Model model) {
+        Vehicle vehicle = new Vehicle();
+        model.addAttribute(vehicle);
+        return "newVehicle";
+    }
+
+    /**
+     * Creates a new Vehicle object given provided data
+     *
+     * Returns one of the following status codes:
+     * 201: successfully created a new specimen
+     * 409: unable to create specimen because it already exists
+     *
+     * @param vehicle a JSON representation of a vehicle object
+     * @return the newly created vehicle object
+     */
+    @PostMapping(value = "/createVehicle", consumes = "application/json", produces = "application/json")
+    public Vehicle createVehicle(@RequestBody Vehicle vehicle) {
+        return vehicle;
+    }
 }
