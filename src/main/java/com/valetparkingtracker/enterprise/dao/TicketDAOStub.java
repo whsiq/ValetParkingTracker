d462f427855c4daa2b506ccf80a3ff1be99444f3
yutize <yutongc2017@gmail.com>
2024-10-20 18:15:34 -0400

    Ticket micro service implementation
    Need to check on some errors still but should work

--- src/main/java/com/valetparkingtracker/enterprise/dao/TicketDAOStub.java
+++ src/main/java/com/valetparkingtracker/enterprise/dao/TicketDAOStub.java
@@ -1,4 +1,39 @@
 package com.valetparkingtracker.enterprise.dao;
 
-public class TicketDAOStub {
+import com.valetparkingtracker.enterprise.dto.Customer;
+import com.valetparkingtracker.enterprise.dto.Ticket;
+
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class TicketDAOStub implements ITicketDAO {
+
+    Map<Integer, Ticket> allTickets = new HashMap<>();
+
+    @Override
+    public Ticket save(Ticket ticket) throws Exception {
+        Integer ticketID = Integer.parseInt(ticket.getTicketId());
+        allTickets.put(ticketID, ticket);
+        return ticket;
+    }
+
+    @Override
+    public List<Ticket> fetchAll() {
+        List<Ticket> returnTickets = new ArrayList(allTickets.values());
+        return returnTickets;
+    }
+
+    @Override
+    public Ticket fetch(int id) {
+        return allTickets.get(id);
+    }
+
+    @Override
+    public void delete(int id) {
+        allTickets.remove(id);
+    }
 }
