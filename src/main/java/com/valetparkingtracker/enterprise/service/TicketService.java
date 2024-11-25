7dde904dfb08f2846eda1acfdaec42808ad569c9
yutize <yutongc2017@gmail.com>
2024-11-24 20:36:35 -0500

    implement cache

--- src/main/java/com/valetparkingtracker/enterprise/service/TicketService.java
+++ src/main/java/com/valetparkingtracker/enterprise/service/TicketService.java
@@ -3,6 +3,8 @@ package com.valetparkingtracker.enterprise.service;
 import com.valetparkingtracker.enterprise.dao.ITicketDAO;
 import com.valetparkingtracker.enterprise.dto.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -19,12 +21,14 @@ public class TicketService implements ITicketService {
     }
 
     @Override
+    @Cacheable(value = "ticket", key = "#id")
     public Ticket fetchById(int id) {
         Ticket foundTicket = ticketDAO.fetch(id);
         return foundTicket;
     }
 
     @Override
+    @CacheEvict(value = "ticket", key = "#id")
     public void delete(int id) throws Exception {
         ticketDAO.delete(id);
     }
@@ -35,6 +39,7 @@ public class TicketService implements ITicketService {
     }
 
     @Override
+    @Cacheable("tickets")
     public List<Ticket> fetchAll() {
         return ticketDAO.fetchAll();
     }
