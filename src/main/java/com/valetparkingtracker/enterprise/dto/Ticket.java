0f6fa7e392578303f3815347bc4c189ac0a02c88
yutize <yutongc2017@gmail.com>
2024-11-24 20:27:51 -0500

    DB Entity

--- src/main/java/com/valetparkingtracker/enterprise/dto/Ticket.java
+++ src/main/java/com/valetparkingtracker/enterprise/dto/Ticket.java
@@ -1,11 +1,18 @@
 package com.valetparkingtracker.enterprise.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 // Ticket can likely be a JSON in/out object - think of the objects being sent from valet computer to front desk computer , or backup to valet computer
 // give properties @SerializedName tag
-@Data
-public class Ticket {
+
+@Entity
+public @Data class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int ticketId;
     private Customer customer;
     private Vehicle vehicle;
